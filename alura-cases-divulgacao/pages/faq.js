5a865c1c21e093d2abd33000582ba25f966e81d4
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2024-05-01 15:36:41 -0300

    Link

--- alura-cases-divulgacao/pages/faq.js
+++ alura-cases-divulgacao/pages/faq.js
@@ -1,8 +1,12 @@
+import Link from 'next/link'
+
 export default function Home() {
     return (
         <div>
             <h1>FAQ</h1>
-            <a href="/">Ir para o home</a>
+            <Link href="/">
+                Ir para o home
+            </Link> 
         </div>
     )
 }
