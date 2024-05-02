33c88329ab377f4d780709875e2e75c969ebcf9a
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2024-05-02 13:41:16 -0300

    Estilos no next js

--- alura-cases-divulgacao/pages/index.js
+++ alura-cases-divulgacao/pages/index.js
@@ -1,9 +1,24 @@
 import Link from "next/link";
 
+function Title({ children, as }) {
+    const Tag = as
+    return (
+        <>
+            <Tag>{children}</Tag>
+            <style jsx>{`
+                ${Tag} {
+                    color: red;
+                    font-family: sans-serif
+                } 
+            `}</style>
+        </>
+    )
+}
+
 export default function Home() {
     return (
         <div>
-            <h1>Alura Cases - Home</h1>
+            <Title as="h1" >Alura Cases - Home</Title>
             <Link href="/faq">
                 Ir para o faq
             </Link>
