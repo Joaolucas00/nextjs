be6d6d8584e43006a9bb043992605b9c04254eb9
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2024-05-20 16:02:55 -0300

    Formulário

--- autenticacao/next-3-jwt-projeto_inicial/frontend/pages/index.js
+++ autenticacao/next-3-jwt-projeto_inicial/frontend/pages/index.js
@@ -1,21 +1,43 @@
+import { useState } from 'react'
+import { useRouter } from 'next/router'
+
 export default function HomeScreen() {
+  const router = useRouter()
+  const [values, setValues] = useState({
+    usuario: 'Joao',
+    senha: 'MinhaSenha4123'
+  })
+
+  const handlerChange = (event) => {
+    const fieldValue = event.target.value
+    const fieldName = event.target.name
+    setValues((currentValues) => {
+      return {
+        ...currentValues,
+        [fieldName]: fieldValue
+      }
+    })
+  }
+
+
   return (
     <div>
       <h1>Login</h1>
-      <form>
-        <input
-          placeholder="Usuário" name="usuario"
-          defaultValue="omariosouto"
-        />
-        <input
-          placeholder="Senha" name="senha" type="password"
-          defaultValue="safepassword"
-        />
+      <form onSubmit={(e) => {
+        e.preventDefault()
+        router.push('/auth-page-static')
+        router.push('/auth-page-ssr')
+      }}>
+        <input placeholder="Usuário" name="usuario" value={values.usuario} onChange={handlerChange}/>
+        <input placeholder="Senha" name="senha" type="password" value={values.senha} onChange={handlerChange} />
         <div>
           <button>
             Entrar
           </button>
         </div>
+        <pre>
+          {JSON.stringify(values, null, 2)}
+        </pre>
       </form>
     </div>
   );
