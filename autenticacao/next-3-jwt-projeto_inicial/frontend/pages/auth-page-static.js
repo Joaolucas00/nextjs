e51fe5e0b334e3b4155c56104fd7edc463451730
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2024-05-28 16:31:30 -0300

    hoc

--- autenticacao/next-3-jwt-projeto_inicial/frontend/pages/auth-page-static.js
+++ autenticacao/next-3-jwt-projeto_inicial/frontend/pages/auth-page-static.js
@@ -1,52 +1,16 @@
-import { useEffect, useState } from "react"
-import { authService } from "../src/services/auth/authServices"
-import { useRouter } from "next/router"
-
-const useSession = () => {
-    const [session, setSession] = useState(null)
-    const [loading, setLoading] = useState(true)
-    const [error, setError] = useState(null)
-
-    useEffect(() => {
-        authService.getSession()
-        .then((res) => {
-            setSession(res)
-        }).catch((err) => {
-            setError(err)
-        }).finally(() => {
-            setLoading(false)
-        })
-    }, [])
-
-    return {
-        data: {
-            session
-        },
-        error,
-        loading,
-    }
-}
-
+import { withSessionHOC } from "../src/services/auth/session"
 
 
 
 const AuthPageStatic = (props) => {
-
-    const session = useSession()
-    const rota = useRouter()
-
-    if (!session.loading && session.error) {
-        rota.push('/?error=unauthorized&status=401')
-    }
-
     return (
         <div>
             <h1>Auth Page Static</h1>
         <pre>
-            {JSON.stringify(session, null, 2)}
+            {JSON.stringify(props, null, 2)}
         </pre>
         </div>
     )
 }
 
-export default AuthPageStatic
+export default withSessionHOC(AuthPageStatic)
--- autenticacao/next-3-jwt-projeto_inicial/frontend/src/services/auth/session.js
+++ autenticacao/next-3-jwt-projeto_inicial/frontend/src/services/auth/session.js
@@ -1,4 +1,6 @@
 import { authService } from "./authServices"
+import { useEffect, useState } from "react"
+import { useRouter } from "next/router"
 
 // Decorator Patterne 
 export const withSession = (callback) => {
@@ -21,5 +23,48 @@ export const withSession = (callback) => {
                 }
             }
         }
+    }
+}
+
+
+export const useSession = () => {
+    const [session, setSession] = useState(null)
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
+
+    useEffect(() => {
+        authService.getSession()
+        .then((res) => {
+            setSession(res)
+        }).catch((err) => {
+            setError(err)
+        }).finally(() => {
+            setLoading(false)
+        })
+    }, [])
+
+    return {
+        data: {
+            session
+        },
+        error,
+        loading,
+    }
+}
+
+export function withSessionHOC(Component) {
+    return function Wrapper(props) {
+        const session = useSession()
+        const rota = useRouter()
+        if (!session.loading && session.error) {
+            rota.push('/?error=unauthorized&status=401')
+        }
+        const modifiedProps = {
+            ...props,
+            session: session.data.session
+        }
+        return (
+            <Component {...modifiedProps} />
+        )
     }
 }
