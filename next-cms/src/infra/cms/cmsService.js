a62eefbef413cd0ce3f79d45d9a06e55c3d8fb91
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2024-06-13 19:00:20 -0300

    Update cmsService.js

--- next-cms/src/infra/cms/cmsService.js
+++ next-cms/src/infra/cms/cmsService.js
@@ -1,3 +1,11 @@
+const globalQuery = `
+    query {
+    globalFooter {
+        description
+    }
+    }
+`
+
 export async function cmsService({ query }) {
 
     try {
@@ -13,14 +21,35 @@ export async function cmsService({ query }) {
         })
         .then(async (res) => {
             const body = await res.json()
-            console.log(body);
+            if(!body.errors) return body
+
+            throw new Error(JSON.stringify(body))
+        })
+
+        const globalContentResponse = await fetch('https://graphql.datocms.com/', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+                'Authorization': 'Bearer ' + process.env.TOKEN_CMS
+            },
+            body: JSON.stringify({
+                query: globalQuery,
+            })
+        })
+        .then(async (res) => {
+            const body = await res.json()
             if(!body.errors) return body
 
             throw new Error(JSON.stringify(body))
         })
     
         return {
-            data: pageContentResponse.data
+            data: {
+                ...pageContentResponse.data,
+                globalContent: {
+                    ...globalContentResponse.data
+                }
+            }
         }
     } catch (error) {
         throw new Error(error.message)
