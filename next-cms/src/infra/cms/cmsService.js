a4588251b8c229f4d85f983c5260a62ef788c958
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2024-06-13 15:05:34 -0300

    Update cmsService.js

--- next-cms/src/infra/cms/cmsService.js
+++ next-cms/src/infra/cms/cmsService.js
@@ -1,5 +1,22 @@
-export async function cmsService(query) {
+export async function cmsService({ query }) {
+
+    const pageContentResponse = await fetch('https://graphql.datocms.com/', {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+        },
+        body: JSON.stringify({
+            query,
+        })
+    })
+    .then(async (res) => {
+        const body = await res.json()
+        return body;
+    })
+
+    console.log('pageContentResponse', pageContentResponse);
+
     return {
-        data: {}
+        data: pageContentResponse
     }
 }
