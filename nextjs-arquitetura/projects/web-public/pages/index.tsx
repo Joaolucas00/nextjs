d6716398cb9dafb190276b2bff550d74ebf6831b
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2024-06-19 17:47:01 -0300

    yarn workspace

--- nextjs-arquitetura/projects/web-public/pages/index.tsx
+++ nextjs-arquitetura/projects/web-public/pages/index.tsx
@@ -1,10 +1,14 @@
 import { sum } from "@alura/utils/math/sum"
+import { Text } from "@alura/design-system/components/Text"
 
 export default function Home() {
     return (
         <main>
             <h1>Home</h1>
-            <p>Importando modulo local: @alura/utils/math/sum sum(2, 2): { sum(2, 2) } </p>
+            <Text tag="p">Importando modulo local: @alura/utils/math/sum sum(2, 2): { sum(2, 2) } </Text>
+            <Text tag="h1">
+              design-system
+            </Text>
         </main>
     )
 }
