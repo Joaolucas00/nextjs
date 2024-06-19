cef243ed31578ef011da3bdb6eb09a7898cbb14c
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2024-06-19 17:05:28 -0300

    mono-repo é muito bom!

--- nextjs-arquitetura/projects/web-public/pages/index.tsx
+++ nextjs-arquitetura/projects/web-public/pages/index.tsx
@@ -1,7 +1,10 @@
+import { sum } from "@alura/utils/math/sum"
+
 export default function Home() {
     return (
         <main>
             <h1>Home</h1>
+            <p>Importando modulo local: @alura/utils/math/sum sum(2, 2): { sum(2, 2) } </p>
         </main>
     )
 }
