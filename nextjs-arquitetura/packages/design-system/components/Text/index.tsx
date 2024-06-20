84a3fccfd2228e8caeccdffbfeabdc1f6e747593
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2024-06-20 16:02:05 -0300

    Configurações do ESLint

--- nextjs-arquitetura/packages/design-system/components/Text/index.tsx
+++ nextjs-arquitetura/packages/design-system/components/Text/index.tsx
@@ -1,18 +1,18 @@
-import styled from 'styled-components'
-import React from 'react'
+import styled from "styled-components";
+import React from "react";
 
 const StyledText = styled.span`
   font-family: sans-serif;
-`
+`;
 interface TextProps {
-  tag: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'span' | 'p'
-  children: React.ReactNode
+  tag: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "span" | "p";
+  children: React.ReactNode;
 }
 
-export const Text = ({ tag, children, ...props}: TextProps) => {
+export const Text = ({ tag, children, ...props }: TextProps) => {
   return (
     <StyledText as={tag} {...props}>
-      { children }
+      {children}
     </StyledText>
-  )
-}
+  );
+};
--- nextjs-arquitetura/projects/web-public/pages/index.tsx
+++ nextjs-arquitetura/projects/web-public/pages/index.tsx
@@ -1,14 +1,14 @@
-import { sum } from "@alura/utils/math/sum"
-import { Text } from "@alura/design-system/components/Text"
+import { sum } from "@alura/utils/math/sum";
+import { Text } from "@alura/design-system/components/Text";
 
 export default function Home() {
-    return (
-        <main>
-            <h1>Home</h1>
-            <Text tag="p">Importando modulo local: @alura/utils/math/sum sum(2, 2): { sum(2, 2) } </Text>
-            <Text tag="h1">
-              design-system
-            </Text>
-        </main>
-    )
+  return (
+    <main>
+      <h1>Home</h1>
+      <Text tag="p">
+        Importando modulo local: @alura/utils/math/sum sum(2, 2): {sum(2, 2)}{" "}
+      </Text>
+      <Text tag="h1">design-system</Text>
+    </main>
+  );
 }
